145029c3f135b38947ed2483814dc48c7d183d7a
Edward Heaver <heaver.edward02@gmail.com>
2020-02-22 21:41:24 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,31 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    if params[:artist_id]
+      if Artist.find_by_id(params[:artist_id]) == nil
+        flash[:alert] = "Artist not found."
+        redirect_to artists_path
+      else
+        @songs = Artist.find(params[:artist_id]).songs
+      end
+    else
+      @songs = Song.all
+    end
   end
 
   def show
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      if Artist.find_by_id(params[:artist_id]) != nil
+        @artist = Artist.find_by_id(params[:artist_id])
+        if @artist.songs.find_by_id(params[:id]) == nil
+          flash[:alert] = "Song not found"
+          redirect_to artist_songs_path(@artist)
+        else
+          @song = @artist.songs.find_by_id(params[:id])
+        end
+      end
+    else
+      @song = Song.find_by_id(params[:id])
+    end
   end
 
   def new
